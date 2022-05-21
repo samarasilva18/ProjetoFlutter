f9adac286aa4f9aa442e0164ebf13e8a46486ed6
SAMARA SILVA <1090481921021@fatec-srv.local>
2022-05-21 12:26:20 -0300

    Aula do dia 21\05

--- bank/lib/main.dart
+++ bank/lib/main.dart
@@ -3,15 +3,50 @@ import 'package:flutter/material.dart';
 void main() => runApp(
   MaterialApp(
   home: Scaffold(
-    body: Card(
-      child: Text('teste'),
-      ),
+    body: ListaTransferencia(),
     appBar: AppBar(
       title: Text('transferencias'),
     ),
-    floatingActionButton: FloatingActionButton(child: Icon(Icons.add),
+    floatingActionButton: FloatingActionButton(
+      child: Icon(Icons.add),
     onPressed: () {},
     ),
     ),
   ),
 );
+
+class Transferencia {
+  final double valor;
+  final int numeroConta;
+
+  Transferencia(this.valor, this.numeroConta);
+}
+
+class ItemTransferencia extends StatelessWidget {
+  final Transferencia _transferencia;
+  
+  ItemTransferencia(this._transferencia);
+
+  @override
+  Widget build(BuildContext context) {  
+    return Card(
+      child: ListTile(
+        leading: Icon(Icons.monetization_on),
+        title: Text(_transferencia.valor.toString()),
+        subtitle: Text(_transferencia.numeroConta.toString()),
+      ),
+    );
+  }
+}
+
+class ListaTransferencia extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(children: [
+      ItemTransferencia(Transferencia(100, 1234)),
+      ItemTransferencia(Transferencia(200, 1111)),
+      ItemTransferencia(Transferencia(1000, 6843-8)),
+    ],
+    );
+  }
+}
